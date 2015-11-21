253f8e12263255d99cfc5c30fd4b6f55a0cb1d46
Benjy Cui <benjytrys@gmail.com>
2015-11-21 10:18:26 +0800

    chore: deprecate `classSet` `joinClasses`

--- src/joinClasses.js
+++ src/joinClasses.js
@@ -1,27 +1,4 @@
-/**
- * Combines multiple className strings into one.
- * http://jsperf.com/joinclasses-args-vs-array
- *
- * @param {...?string} classes
- * @return {string}
- */
+import deprecate from 'util-deprecate';
+import classNames from 'classnames';
 
-function joinClasses(cn) {
-  let className = cn;
-  if (!className) {
-    className = '';
-  }
-  let nextClass;
-  const argLength = arguments.length;
-  if (argLength > 1) {
-    for (let ii = 1; ii < argLength; ii++) {
-      nextClass = arguments[ii];
-      if (nextClass) {
-        className = (className ? className + ' ' : '') + nextClass;
-      }
-    }
-  }
-  return className;
-}
-
-module.exports = joinClasses;
+export default deprecate(classNames, '`rcUtil.joinClasses()` is deprecated, use `classNames()` by `require(\'classnames\')` instead');
