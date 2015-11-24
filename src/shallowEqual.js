b05097dab30a1e920e4969c86739d8e12f22de5f
yiminghe <yiminghe@gmail.com>
2015-11-24 23:48:02 +0800

    do not deprecate shallowEqual

--- src/shallowEqual.js
+++ src/shallowEqual.js
@@ -1,4 +1,3 @@
 const shallowEqual = require('shallowequal');
-const deprecate = require('util-deprecate');
 
-module.exports = deprecate(shallowEqual, '`rcUtil.shallowEqual()` is deprecated, use `shallowEqual()` by `require(\'shallowequal\')` instead');
+module.exports = shallowEqual;
