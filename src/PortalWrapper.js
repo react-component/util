9bd7e4ad753edbde82c2718ee9407041633f0904
jljsj <liangjun.jiang@alipay.com>
2019-07-04 11:40:27 +0800

    add protal wrapper and getScroll func

---
+++ src/PortalWrapper.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import ContainerRender from './ContainerRender';
+import Portal from './Portal';
+
+let openCount = 0;
+const windowIsUndefined = !(
+  typeof window !== 'undefined' &&
+  window.document &&
+  window.document.createElement
+);
+
+const IS_REACT_16 = 'createPortal' in ReactDOM;
+
+
+export default class PortalWrapper extends React.Component {
+  static propTypes = {
+    wrapperClassName: PropTypes.string,
+    forceRender: PropTypes.bool,
+    getContainer: PropTypes.any,
+    children: PropTypes.func,
+    visible: PropTypes.bool,
+  }
+
+  constructor(props) {
+    super(props);
+    const { visible, forceRender } = this.props;
+    const show = visible || forceRender;
+    if (show) {
+      openCount = show ? openCount + 1 : openCount - 1;
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { visible } = nextProps;
+    if (visible !== this.props.visible) {
+      openCount = visible && !this.props.visible ? openCount + 1 : openCount - 1;
+    }
+  }
+  shouldComponentUpdate({ visible, forceRender }) {
+    return !!(this.props.visible || visible) || (this.props.forceRender || forceRender);
+  }
+  componentWillUnmount() {
+    const { visible } = this.props;
+    this.container = null;
+    this._component = null;
+    if (!IS_REACT_16) {
+      if (visible) {
+        this.renderComponent({
+          afterClose: this.removeContainer,
+          onClose() { },
+          visible: false,
+        });
+      } else {
+        this.removeContainer();
+      }
+    }
+  }
+  getParent = () => {
+    const { getContainer } = this.props;
+    if (getContainer) {
+      if (typeof getContainer === 'string') {
+        return document.querySelectorAll(getContainer)[0];
+      }
+      if (typeof getContainer === 'function') {
+        return getContainer();
+      }
+      if (typeof getContainer === 'object' && getContainer instanceof window.HTMLElement) {
+        return getContainer;
+      }
+    }
+    return document.body;
+  }
+  getContainer = () => {
+    if (windowIsUndefined) {
+      return null;
+    }
+    if (!this.container) {
+      this.container = document.createElement('div');
+      const parent = this.getParent();
+      parent.appendChild(this.container);
+      const { wrapperClassName } = this.props;
+      if (wrapperClassName) {
+        this.container.className = wrapperClassName;
+      }
+    }
+    return this.container;
+  }
+
+  savePortal = (c) => {
+    this._component = c;
+  }
+  render() {
+    const { children, forceRender, visible } = this.props;
+    let portal = null;
+    const childProps = {
+      openCount,
+      getContainer: this.getContainer,
+    };
+    // suppport react15
+    if (!IS_REACT_16) {
+      return (
+        <ContainerRender
+          parent={this}
+          visible={visible}
+          autoDestroy={false}
+          getComponent={(extra = {}) => (children({ ...extra, ...childProps }))}
+          getContainer={this.getContainer}
+          forceRender={forceRender}
+        >
+          {({ renderComponent, removeContainer }) => {
+            this.renderComponent = renderComponent;
+            this.removeContainer = removeContainer;
+            return null;
+          }}
+        </ContainerRender>
+      );
+    }
+    if (forceRender || visible || this._component) {
+      portal = (
+        <Portal getContainer={this.getContainer} ref={this.savePortal} >
+          {children(childProps)}
+        </Portal>
+      );
+    }
+    return portal;
+  }
+}
--- src/getScrollBarSize.js
+++ src/getScrollBarSize.js
@@ -36,3 +36,24 @@ export default function getScrollBarSize(fresh) {
   }
   return cached;
 }
+
+export const getScrollBarSizeFunc = () => (
+  document.body.scrollHeight >
+    (window.innerHeight || document.documentElement.clientHeight) &&
+    (window.innerWidth || document.documentElement.clientWidth) > document.body.offsetWidth
+    ? getScrollBarSize(true)// router 切换时可能会导至页面失去滚动条，所以需要时时获取。
+    : 0
+);
+
+export const switchScrollingEffect = (close) => {
+  const scrollBarSize = getScrollBarSizeFunc();
+  if (scrollBarSize) {
+    if (close) {
+      document.body.style.position = '';
+      document.body.style.width = '';
+    } else {
+      document.body.style.position = 'relative';
+      document.body.style.width = `calc(100% - ${scrollBarSize}px)`;
+    }
+  }
+};
