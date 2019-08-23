79f1692f308246c86a31cd47658b81a1a115110a
zombiej <smith3816@gmail.com>
2019-08-23 14:59:39 +0800

    support note

--- src/warning.ts
+++ src/warning.ts
@@ -1,14 +1,42 @@
-import warning from 'warning';
-
+/* eslint-disable no-console */
 let warned: Record<string, boolean> = {};
+
+export function warning(valid: boolean, message: string) {
+  // Support uglify
+  if (process.env.NODE_ENV !== 'production' && !valid && console !== undefined) {
+    console.error(`Warning: ${message}`);
+  }
+}
+
+export function note(valid: boolean, message: string) {
+  // Support uglify
+  if (process.env.NODE_ENV !== 'production' && !valid && console !== undefined) {
+    console.warn(`Note: ${message}`);
+  }
+}
 
 export function resetWarned() {
   warned = {};
 }
 
-export default (valid: boolean, message: string): void => {
+export function call(
+  method: (valid: boolean, message: string) => void,
+  valid: boolean,
+  message: string,
+) {
   if (!valid && !warned[message]) {
-    warning(false, message);
+    method(false, message);
     warned[message] = true;
   }
-};
+}
+
+export function warningOnce(valid: boolean, message: string) {
+  call(warning, valid, message);
+}
+
+export function noteOnce(valid: boolean, message: string) {
+  call(note, valid, message);
+}
+
+export default warningOnce;
+/* eslint-enable */
