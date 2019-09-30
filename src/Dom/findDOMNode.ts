30f034f847cbfb112cce910180326aac15d037e5
zombiej <smith3816@gmail.com>
2019-09-30 11:47:20 +0800

    chore: Update findDOMNode define

--- src/Dom/findDOMNode.ts
+++ src/Dom/findDOMNode.ts
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 /**
  * Return if a node is a DOM node. Else will return by `findDOMNode`
  */
-export default function findDOMNode(node: React.ReactInstance | HTMLElement): Element | Text {
+export default function findDOMNode<T = Element | Text>(
+  node: React.ReactInstance | HTMLElement,
+): T {
   if (node instanceof HTMLElement) {
-    return node;
+    return (node as unknown) as T;
   }
-  return ReactDOM.findDOMNode(node);
+  return (ReactDOM.findDOMNode(node) as unknown) as T;
 }
