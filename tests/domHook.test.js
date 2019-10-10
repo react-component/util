812205ac54744ce7005295ee07a8c14bda08da1f
zombiej <smith3816@gmail.com>
2019-10-10 13:49:35 +0800

    add test case

---
+++ tests/domHook.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable class-methods-use-this, @typescript-eslint/no-empty-function */
+import { spyElementPrototype } from '../src/test/domHook';
+
+describe('domHook', () => {
+  function spyTest(name, propName, hookProp, assetFunc) {
+    it(name, () => {
+      class Test {
+        func() {}
+      }
+
+      // Spy
+      const originFunc = Test.prototype.func;
+      const mock = spyElementPrototype(Test, propName, hookProp);
+
+      const test = new Test();
+      assetFunc(test);
+
+      // Restore
+      mock.mockRestore();
+      expect(test.func).toBe(originFunc);
+    });
+  }
+
+  spyTest('not exist', 'noExist', { get: () => 'bamboo' }, test => {
+    expect(test.noExist).toBe('bamboo');
+  });
+
+  spyTest(
+    'function',
+    'func',
+    () => 'light',
+    test => {
+      expect(test.func()).toBe('light');
+    },
+  );
+
+  spyTest('prop', 'func', { get: () => 'little' }, test => {
+    expect(test.func).toBe('little');
+  });
+});
+/* eslint-enable */
