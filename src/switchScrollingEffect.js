ccbc422c4dae99d962167361d11fa3ac7c185654
shao <darryshaw@gmail.com>
2019-11-14 17:45:57 +0800

    feat: switchScrollingEffect add a class when called

--- src/switchScrollingEffect.js
+++ src/switchScrollingEffect.js
@@ -1,20 +1,31 @@
 import getScrollBarSize from './getScrollBarSize';
 
-export default (close) => {
-  const bodyIsOverflowing = document.body.scrollHeight >
-    (window.innerHeight || document.documentElement.clientHeight) &&
+export default close => {
+  const bodyIsOverflowing =
+    document.body.scrollHeight > (window.innerHeight || document.documentElement.clientHeight) &&
     window.innerWidth > document.body.offsetWidth;
   if (!bodyIsOverflowing) {
     return;
   }
+
+  // https://github.com/ant-design/ant-design/issues/19729
+  const scrolllingEffectClassName = 'scroolling-effect';
+  const scrolllingEffectClassNameReg = new RegExp(`(^|\\s)${scrolllingEffectClassName}($|\\s)`);
+  const bodyClassName = document.body.className;
   if (close) {
     document.body.style.position = '';
     document.body.style.width = '';
+    if (scrolllingEffectClassNameReg.test(bodyClassName)) {
+      document.body.className = bodyClassName.replace(scrolllingEffectClassNameReg, '');
+    }
     return;
   }
   const scrollBarSize = getScrollBarSize();
   if (scrollBarSize) {
     document.body.style.position = 'relative';
     document.body.style.width = `calc(100% - ${scrollBarSize}px)`;
+    if (!scrolllingEffectClassNameReg.test(bodyClassName)) {
+      document.body.className = `${bodyClassName} ${scrolllingEffectClassName}`;
+    }
   }
 };
