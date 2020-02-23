12e6a239bac5a5318cbe1e70e43a1f009640f717
二货机器人 <smith3816@gmail.com>
2020-02-23 17:31:00 +0800

    feat: toArray support Fragment (#84)
    * feat: Support fragement
    
    * use react-is

--- src/Children/toArray.ts
+++ src/Children/toArray.ts
@@ -1,9 +1,18 @@
 import React from 'react';
+import { isFragment } from 'react-is';
 
-export default function toArray(children: React.ReactNode): React.ReactElement[] {
-  const ret = [];
-  React.Children.forEach(children, c => {
-    ret.push(c);
+export default function toArray(
+  children: React.ReactNode,
+): React.ReactElement[] {
+  let ret: React.ReactElement[] = [];
+
+  React.Children.forEach(children, (child: any) => {
+    if (isFragment(child) && child.props) {
+      ret = ret.concat(toArray(child.props.children));
+    } else {
+      ret.push(child);
+    }
   });
+
   return ret;
 }
