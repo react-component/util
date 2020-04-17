cd7bbb1a702cfe24a4287e21685b43276f45a32f
liangyejuan <liangyejuan@huya.com>
2020-04-17 17:36:05 +0800

    try to fixed coverage again

--- src/getScrollBarSize.js
+++ src/getScrollBarSize.js
@@ -23,7 +23,7 @@ export default function getScrollBarSize(fresh, outerClassName = '') {
     outerStyle.overflow = 'hidden';
     
     if (outerClassName) {
-      outer.className = outerClassName;
+      outer.setAttribute('class', outerClassName);
     }
 
     outer.appendChild(inner);
