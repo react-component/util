5c27fc0106ef8cac573c3bd12b64a54309612893
zombiej <smith3816@gmail.com>
2020-07-04 00:10:06 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,10 @@
-const base = require('@umijs/fabric/dist/eslint');
-
 const config = {
-  ...base,
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   rules: {
-    ...base.rules,
     'react/no-did-update-set-state': 0,
     'react/no-find-dom-node': 0,
     'import/no-extraneous-dependencies': 0,
   },
-};
-
-config.parserOptions = {
-  project: './node_modules/father-build/template/tsconfig.json',
 };
 
 module.exports = config;
