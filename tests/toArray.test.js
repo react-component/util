68906806330cd7b30edd7accd595cfa26d64f754
二货机器人 <smith3816@gmail.com>
2020-08-28 10:54:02 +0800

    feat: toArray support empty (#141)

--- tests/toArray.test.js
+++ tests/toArray.test.js
@@ -51,4 +51,40 @@ describe('toArray', () => {
     expect(children).toHaveLength(5);
     expect(children.map(c => c.key)).toEqual(['1', '2', '3', '4', '5']);
   });
+
+  it('keep empty', () => {
+    const wrapper = mount(
+      <ul>
+        {null}
+        <li key="1">1</li>
+        <>
+          <li key="2">2</li>
+          {null}
+          <li key="3">3</li>
+        </>
+        <React.Fragment>
+          <>
+            <li key="4">4</li>
+            {undefined}
+            <li key="5">5</li>
+          </>
+        </React.Fragment>
+        {undefined}
+      </ul>,
+    );
+
+    const children = toArray(wrapper.props().children, { keepEmpty: true });
+    expect(children).toHaveLength(9);
+    expect(children.map(c => c && c.key)).toEqual([
+      null,
+      '1',
+      '2',
+      null,
+      '3',
+      '4',
+      null,
+      '5',
+      null,
+    ]);
+  });
 });
