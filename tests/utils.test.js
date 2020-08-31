129920385071210a00670a038bed29de6e2d07d1
zombiej <smith3816@gmail.com>
2020-08-31 10:39:29 +0800

    add test case

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -51,36 +51,47 @@ describe('utils', () => {
     ]);
   });
 
-  it('pickAttrs', () => {
-    expect(
-      pickAttrs({
-        onClick: null,
-        checked: true,
-        'data-my': 1,
-        'aria-this': 2,
-        skip: true,
-        role: 'button',
-      }),
-    ).toEqual({
+  describe('pickAttrs', () => {
+    const originProps = {
       onClick: null,
       checked: true,
       'data-my': 1,
       'aria-this': 2,
+      skip: true,
       role: 'button',
+    };
+
+    it('default', () => {
+      expect(pickAttrs(originProps)).toEqual({
+        onClick: null,
+        checked: true,
+        'data-my': 1,
+        'aria-this': 2,
+        role: 'button',
+      });
     });
 
-    expect(
-      pickAttrs(
-        {
-          onClick: null,
-          checked: true,
-          'data-my': 1,
-          'aria-this': 2,
-          skip: true,
-          role: 'button',
-        },
-        true,
-      ),
-    ).toEqual({ 'aria-this': 2, role: 'button' });
+    it('aria only', () => {
+      expect(pickAttrs(originProps, true)).toEqual({
+        'aria-this': 2,
+        role: 'button',
+      });
+    });
+
+    it('attr only', () => {
+      expect(pickAttrs(originProps, { attr: true })).toEqual({
+        onClick: null,
+        checked: true,
+        role: 'button',
+      });
+    });
+
+    it('aria & data', () => {
+      expect(pickAttrs(originProps, { aria: true, data: true })).toEqual({
+        'data-my': 1,
+        'aria-this': 2,
+        role: 'button',
+      });
+    });
   });
 });
