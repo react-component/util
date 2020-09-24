bfff63dabc7fa0698d6d1077cabd57534c17d545
zombiej <smith3816@gmail.com>
2020-09-24 12:11:25 +0800

    feat: Add isVisible

---
+++ src/Dom/isVisible.ts
@@ -0,0 +1,18 @@
+export default (element: HTMLElement | SVGGraphicsElement): boolean => {
+  if (!element) {
+    return false;
+  }
+
+  if ((element as HTMLElement).offsetParent) {
+    return true;
+  }
+
+  if ((element as SVGGraphicsElement).getBBox) {
+    const box = (element as SVGGraphicsElement).getBBox();
+    if (box.width || box.height) {
+      return true;
+    }
+  }
+
+  return false;
+};
