bac008eaf319a13f83f266ed87d334c7d5535572
zombiej <smith3816@gmail.com>
2020-11-03 21:45:06 +0800

    feat: add isMobile func

--- src/warn.js
+++ src/warn.js
@@ -1,3 +1,4 @@
+/** @deprecated Use `warning` instead. This will be removed in next major version */
 export default function warn(msg) {
   if (process.env.NODE_ENV !== 'production') {
     if (typeof console !== 'undefined' && console.warn) {
