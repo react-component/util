8489cd180af33e0eab9c2b6a0628217e70f72ea3
二货机器人 <smith3816@gmail.com>
2021-02-04 17:37:40 +0800

    feat: set support remove (#199)
    * feat: set support remove
    
    * test: Update test case
    
    * clean up

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -19,36 +19,83 @@ describe('utils', () => {
     );
   });
 
-  it('set', () => {
-    expect(set(null, [], 233)).toEqual(233);
-    expect(set({}, ['light'], 'bamboo')).toEqual({ light: 'bamboo' });
-    expect(set({}, ['light', 'bamboo'], 'generate')).toEqual({
-      light: { bamboo: 'generate' },
-    });
-    expect(
-      set({ other: { next: 233 } }, ['light', 'bamboo'], 'generate'),
-    ).toEqual({
-      other: { next: 233 },
-      light: { bamboo: 'generate' },
+  describe('set', () => {
+    it('basic', () => {
+      expect(set(null, [], 233)).toEqual(233);
+      expect(set({}, ['light'], 'bamboo')).toEqual({ light: 'bamboo' });
+      expect(set({}, ['light', 'bamboo'], 'generate')).toEqual({
+        light: { bamboo: 'generate' },
+      });
+      expect(
+        set({ other: { next: 233 } }, ['light', 'bamboo'], 'generate'),
+      ).toEqual({
+        other: { next: 233 },
+        light: { bamboo: 'generate' },
+      });
+
+      expect(set([0, 1, 2], [1, 'light', 'bamboo'], 'next')).toEqual([
+        0,
+        { light: { bamboo: 'next' } },
+        2,
+      ]);
+      expect(
+        set({ light: 'bamboo', list: [0, 1, 2] }, ['list', '1'], 233),
+      ).toEqual({
+        light: 'bamboo',
+        list: [0, 233, 2],
+      });
+      expect(set([[[[[0]]]]], [0, 0, 0, 0, 0], 'light')).toEqual([
+        [[[['light']]]],
+      ]);
+      expect(set([[[[[0]]]]], [0, 0, 0, 0, 0, 0], 'bamboo')).toEqual([
+        [[[[['bamboo']]]]],
+      ]);
     });
 
-    expect(set([0, 1, 2], [1, 'light', 'bamboo'], 'next')).toEqual([
-      0,
-      { light: { bamboo: 'next' } },
-      2,
-    ]);
-    expect(
-      set({ light: 'bamboo', list: [0, 1, 2] }, ['list', '1'], 233),
-    ).toEqual({
-      light: 'bamboo',
-      list: [0, 233, 2],
+    it('remove if undefined', () => {
+      // Skip not exist path
+      expect(set({}, ['notExist'], undefined, true)).toEqual({});
+
+      // Delete value
+      const target = set(
+        { keep: { light: 2333 } },
+        ['keep', 'light'],
+        undefined,
+        true,
+      );
+      expect(target).toEqual({ keep: {} });
+      expect('light' in target.keep).toBeFalsy();
+
+      // Mid path not exist
+      const midTgt = set(
+        { lv1: { lv2: {} } },
+        ['lv1', 'lv2', 'lv3'],
+        undefined,
+        true,
+      );
+      expect(midTgt).toEqual({ lv1: { lv2: {} } });
+      expect('lv3' in midTgt.lv1.lv2).toBeFalsy();
+
+      // Long path not exist
+      const longNotExistTgt = set(
+        { lv1: { lv2: {} } },
+        ['lv1', 'lv2', 'lv3', 'lv4'],
+        undefined,
+        true,
+      );
+      expect(longNotExistTgt).toEqual({ lv1: { lv2: {} } });
+      expect('lv3' in longNotExistTgt.lv1.lv2).toBeFalsy();
+
+      // Long path remove
+      const longTgt = set(
+        { lv1: { lv2: { lv3: { lv4: { lv: 5 } } } } },
+        ['lv1', 'lv2', 'lv3', 'lv4'],
+        undefined,
+        true,
+      );
+      expect(longTgt).toEqual({ lv1: { lv2: { lv3: {} } } });
+      expect('lv4' in longTgt.lv1.lv2.lv3).toBeFalsy();
     });
-    expect(set([[[[[0]]]]], [0, 0, 0, 0, 0], 'light')).toEqual([
-      [[[['light']]]],
-    ]);
-    expect(set([[[[[0]]]]], [0, 0, 0, 0, 0, 0], 'bamboo')).toEqual([
-      [[[[['bamboo']]]]],
-    ]);
   });
 
   describe('pickAttrs', () => {
