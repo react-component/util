6cbcbd8746017fcc14583cccde7184d9d8e895f9
zombiej <smith3816@gmail.com>
2021-04-26 19:21:22 +0800

    chore: Move focus to ts

--- src/Dom/focus.ts
+++ src/Dom/focus.ts
@@ -1,62 +1,72 @@
-function hidden(node) {
+function hidden(node: HTMLElement) {
   return node.style.display === 'none';
 }
 
-function visible(node) {
-  while (node) {
-    if (node === document.body) {
+function visible(node: HTMLElement) {
+  let current = node;
+
+  while (current) {
+    if (current === document.body) {
       break;
     }
-    if (hidden(node)) {
+    if (hidden(current)) {
       return false;
     }
-    node = node.parentNode;
+    current = current.parentNode as HTMLElement;
   }
   return true;
 }
 
-function focusable(node) {
+function focusable(node: HTMLElement): boolean {
   const nodeName = node.nodeName.toLowerCase();
   const tabIndex = parseInt(node.getAttribute('tabindex'), 10);
-  const hasTabIndex = !isNaN(tabIndex) && tabIndex > -1;
+  const hasTabIndex = !Number.isNaN(tabIndex) && tabIndex > -1;
 
   if (visible(node)) {
     if (['input', 'select', 'textarea', 'button'].indexOf(nodeName) > -1) {
-      return !node.disabled;
-    } else if (nodeName === 'a') {
-      return (node.getAttribute('href') || hasTabIndex);
+      return !(node as any).disabled;
     }
+
+    if (nodeName === 'a') {
+      return !!node.getAttribute('href') || hasTabIndex;
+    }
+
     return node.isContentEditable || hasTabIndex;
   }
+
+  return false;
 }
 
-export function getFocusNodeList(node) {
-  const res = [].slice.call(node.querySelectorAll('*'), 0).filter((child) => {
-    return focusable(child);
+export function getFocusNodeList(node: HTMLElement) {
+  const res = [...node.querySelectorAll('*')].filter(child => {
+    return focusable(child as HTMLElement);
   });
   if (focusable(node)) {
     res.unshift(node);
   }
-  return res;
+  return res as HTMLElement[];
 }
 
 let lastFocusElement = null;
 
+/** @deprecated Do not use since this may failed when used in async */
 export function saveLastFocusNode() {
   lastFocusElement = document.activeElement;
 }
 
+/** @deprecated Do not use since this may failed when used in async */
 export function clearLastFocusNode() {
   lastFocusElement = null;
 }
 
+/** @deprecated Do not use since this may failed when used in async */
 export function backLastFocusNode() {
   if (lastFocusElement) {
     try {
       // 元素可能已经被移动了
       lastFocusElement.focus();
 
-    /* eslint-disable no-empty */
+      /* eslint-disable no-empty */
     } catch (e) {
       // empty
     }
@@ -64,11 +74,12 @@ export function backLastFocusNode() {
   }
 }
 
-export function limitTabRange(node, e) {
+export function limitTabRange(node: HTMLElement, e: KeyboardEvent) {
   if (e.keyCode === 9) {
     const tabNodeList = getFocusNodeList(node);
     const lastTabNode = tabNodeList[e.shiftKey ? 0 : tabNodeList.length - 1];
-    const leavingTab = (lastTabNode === document.activeElement || node === document.activeElement);
+    const leavingTab =
+      lastTabNode === document.activeElement || node === document.activeElement;
 
     if (leavingTab) {
       const target = tabNodeList[e.shiftKey ? tabNodeList.length - 1 : 0];
