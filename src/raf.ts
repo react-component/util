0d48ccd00c315f1c79e5878038e0163bc5de081d
lijianan <574980606@qq.com>
2022-11-19 17:19:02 +0800

    style: code optimization

--- src/raf.ts
+++ src/raf.ts
@@ -1,20 +1,22 @@
-let raf = (callback: FrameRequestCallback) => +setTimeout(callback, 16);
+const INTERVAL = 1000 / 60;
+
+let raf = (callback: FrameRequestCallback) => setTimeout(callback, INTERVAL);
 let caf = (num: number) => clearTimeout(num);
 
 if (typeof window !== 'undefined' && 'requestAnimationFrame' in window) {
-  raf = (callback: FrameRequestCallback) =>
-    window.requestAnimationFrame(callback);
-  caf = (handle: number) => window.cancelAnimationFrame(handle);
+  raf = (callback: FrameRequestCallback) => requestAnimationFrame(callback);
+  caf = (handle: number) => cancelAnimationFrame(handle);
 }
 
 let rafUUID = 0;
+
 const rafIds = new Map<number, number>();
 
 function cleanup(id: number) {
   rafIds.delete(id);
 }
 
-export default function wrapperRaf(callback: () => void, times = 1): number {
+const wrapperRaf = (callback: () => void, times = 1): number => {
   rafUUID += 1;
   const id = rafUUID;
 
@@ -39,10 +41,12 @@ export default function wrapperRaf(callback: () => void, times = 1): number {
   callRef(times);
 
   return id;
-}
+};
 
 wrapperRaf.cancel = (id: number) => {
   const realId = rafIds.get(id);
   cleanup(realId);
   return caf(realId);
 };
+
+export default wrapperRaf;
