841f2f69314f45a35e25a1970068cd34c296e9d2
栗嘉男 <574980606@qq.com>
2022-12-08 19:40:26 +0800

    add useMutationObserver

---
+++ src/hooks/useMutationObserver.ts
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef } from 'react';
+
+const useMutationObserver = () => {
+  const instance = useRef<MutationObserver>();
+
+  const destroyObserver = () => {
+    if (instance.current) {
+      instance.current.takeRecords();
+      instance.current.disconnect();
+      instance.current = undefined;
+    }
+  };
+
+  const createObserver = (target: Node, callback: MutationCallback) => {
+    if (MutationObserver) {
+      destroyObserver();
+      instance.current = new MutationObserver(callback);
+      instance.current.observe(target, {
+        childList: true,
+        subtree: true,
+        attributeFilter: ['style', 'class'],
+      });
+    }
+  };
+
+  useEffect(() => destroyObserver, []);
+
+  return { createObserver, destroyObserver };
+};
+
+export default useMutationObserver;
