c536bcb87be2c7beb8073f0c07333c6c2d05af8f
栗嘉男 <574980606@qq.com>
2022-12-12 17:17:44 +0800

    update

--- src/omit.ts
+++ src/omit.ts
@@ -1,14 +1,14 @@
-function omit<T extends object, K extends keyof T>(
+export default function omit<T extends object, K extends keyof T>(
   obj: T,
   fields: K[] | readonly K[],
 ): Omit<T, K> {
   const clone = { ...obj };
+
   if (Array.isArray(fields)) {
     fields.forEach(key => {
       delete clone[key];
     });
   }
+
   return clone;
 }
-
-export default omit;
