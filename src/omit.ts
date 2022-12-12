8932391c25900ae5593284366fce4f3b3270b1ac
栗嘉男 <574980606@qq.com>
2022-12-12 16:36:35 +0800

    sync lodash

--- src/omit.ts
+++ src/omit.ts
@@ -1,14 +1,24 @@
-export default function omit<T extends object, K extends keyof T>(
-  obj: T,
-  fields: K[],
-): Omit<T, K> {
-  const clone = { ...obj };
+function omit<T extends object, K extends PropertyKey[]>(
+  obj: T | null | undefined,
+  ...fields: (K | readonly K[])[]
+): Pick<T, Exclude<keyof T, K[number]>>;
 
+function omit<T extends object, K extends keyof T>(
+  obj: T | null | undefined,
+  ...fields: K[]
+): Omit<T, K>;
+
+function omit<T extends object>(
+  obj: T | null | undefined,
+  ...fields: (PropertyKey | readonly PropertyKey[])[]
+): Partial<T> {
+  const clone = { ...obj };
   if (Array.isArray(fields)) {
-    fields.forEach(key => {
+    fields.forEach((key: PropertyKey) => {
       delete clone[key];
     });
   }
-
   return clone;
 }
+
+export default omit;
