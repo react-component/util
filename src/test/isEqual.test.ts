88c1572e41a179f40d5770ddad2e64ba06a3565a
yoyo837 <yoyo837@hotmail.com>
2022-12-16 16:42:50 +0800

    add test cace

--- src/test/isEqual.test.ts
+++ src/test/isEqual.test.ts
@@ -9,6 +9,12 @@ describe('isEqual', () => {
     expect(valueIsEqual).toBe(true);
   });
 
+  const obj = { a: 1, b: 2, c: [1, 2] };
+  it('should equal 2', () => {
+    const valueIsEqual = isEqual(obj, obj);
+    expect(valueIsEqual).toBe(true);
+  });
+
   it('should not equal', () => {
     const valueIsEqual = isEqual(
       { a: 1, b: 2, c: [2, 3] },
