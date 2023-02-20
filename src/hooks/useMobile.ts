d875605a3952448462c4eb65b27fefd3fbe0b200
MadCcc <1075746765@qq.com>
2023-02-20 17:45:51 +0800

    feat: use useLayoutEffect

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import isMobile from '../isMobile';
 
 /**
  * Hook to detect if the user is on a mobile device
- * Notice that this hook will only detect the device type in effect, so it will not work in SSR
+ * Notice that this hook will only detect the device type in effect, so it will work in SSR
  */
 const useMobile = (): boolean => {
   const [mobile, setMobile] = useState(false);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setMobile(isMobile());
   }, []);
 
