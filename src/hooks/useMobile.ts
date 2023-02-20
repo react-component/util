f06cecf117dcbe9982b4a7638ad1699be4c8cf24
MadCcc <1075746765@qq.com>
2023-02-20 17:51:06 +0800

    chore: update comment

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -3,7 +3,7 @@ import isMobile from '../isMobile';
 
 /**
  * Hook to detect if the user is on a mobile device
- * Notice that this hook will only detect the device type in effect, so it will work in SSR
+ * Notice that this hook will only detect the device type in effect, so it will always be false in server side
  */
 const useMobile = (): boolean => {
   const [mobile, setMobile] = useState(false);
