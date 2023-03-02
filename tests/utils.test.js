ee29b9f1cab2582124c5a782ce4df23d405981d2
Robert Kuykendall <robert@rkuykendall.com>
2023-03-02 11:32:13 -0600

    Fix nesting

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -140,11 +140,11 @@ describe('utils', () => {
         role: 'button',
       });
     });
-  });
-    
-  it('data only', () => {
-    expect(pickAttrs(originProps, { data: true })).toEqual({
-      'data-my': 1,
+
+    it('data only', () => {
+      expect(pickAttrs(originProps, { data: true })).toEqual({
+        'data-my': 1,
+      });
     });
-  });      
+  });
 });
