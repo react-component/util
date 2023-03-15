56716c199565423a8ffe743013012d7327290c19
二货机器人 <smith3816@gmail.com>
2023-03-15 14:18:14 +0800

    test: add test case

--- src/Dom/findDOMNode.ts
+++ src/Dom/findDOMNode.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export function isDOM(node: any) {
+export function isDOM(node: any): node is HTMLElement | SVGElement {
   // https://developer.mozilla.org/en-US/docs/Web/API/Element
   // Since XULElement is also subclass of Element, we only need HTMLElement and SVGElement
   return node instanceof HTMLElement || node instanceof SVGElement;
@@ -14,11 +14,11 @@ export default function findDOMNode<T = Element | Text>(
   node: React.ReactInstance | HTMLElement | SVGElement,
 ): T {
   if (isDOM(node)) {
-    return node as unknown as T;
+    return (node as unknown) as T;
   }
 
   if (node instanceof React.Component) {
-    return ReactDOM.findDOMNode(node) as unknown as T;
+    return (ReactDOM.findDOMNode(node) as unknown) as T;
   }
 
   return null;
