6053dffb4946bd225cb14e2856a990f8a12f0003
二货爱吃白萝卜 <smith3816@gmail.com>
2023-05-23 19:28:38 +0800

    chore: patch ts def (#439)

--- src/Dom/shadow.ts
+++ src/Dom/shadow.ts
@@ -2,6 +2,6 @@ export function inShadow(ele: Node) {
   return ele?.getRootNode() !== ele?.ownerDocument;
 }
 
-export function getShadowRoot(ele: Node) {
-  return inShadow(ele) ? ele?.getRootNode() : null;
+export function getShadowRoot(ele: Node): ShadowRoot {
+  return inShadow(ele) ? (ele?.getRootNode() as ShadowRoot) : null;
 }
