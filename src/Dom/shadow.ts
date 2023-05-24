61b1db2a2ad80786fc9bb5e32382c4f4a17da605
二货爱吃白萝卜 <smith3816@gmail.com>
2023-05-24 19:18:42 +0800

    fix: fallback ie (#440)
    * fix: fallback ie
    
    * chore: comment it

--- src/Dom/shadow.ts
+++ src/Dom/shadow.ts
@@ -1,7 +1,17 @@
-export function inShadow(ele: Node) {
-  return ele?.getRootNode() !== ele?.ownerDocument;
+function getRoot(ele: Node) {
+  return ele?.getRootNode?.();
 }
 
+/**
+ * Check if is in shadowRoot
+ */
+export function inShadow(ele: Node) {
+  return getRoot(ele) !== ele?.ownerDocument;
+}
+
+/**
+ * Return shadowRoot if possible
+ */
 export function getShadowRoot(ele: Node): ShadowRoot {
-  return inShadow(ele) ? (ele?.getRootNode() as ShadowRoot) : null;
+  return inShadow(ele) ? (getRoot(ele) as ShadowRoot) : null;
 }
