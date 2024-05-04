e9900faaaac1eba1a240dd9694c2dba627a9fb58
lijianan <574980606@qq.com>
2024-05-04 19:22:57 +0800

    chore: rewrite function => arrow function (#529)

--- src/ref.ts
+++ src/ref.ts
@@ -1,43 +1,41 @@
-/* eslint-disable no-param-reassign */
 import type * as React from 'react';
 import { isValidElement } from 'react';
 import { ForwardRef, isFragment, isMemo } from 'react-is';
 import useMemo from './hooks/useMemo';
 
-export function fillRef<T>(ref: React.Ref<T>, node: T) {
+export const fillRef = <T>(ref: React.Ref<T>, node: T) => {
   if (typeof ref === 'function') {
     ref(node);
   } else if (typeof ref === 'object' && ref && 'current' in ref) {
     (ref as any).current = node;
   }
-}
+};
 
 /**
  * Merge refs into one ref function to support ref passing.
  */
-export function composeRef<T>(...refs: React.Ref<T>[]): React.Ref<T> {
-  const refList = refs.filter(ref => ref);
+export const composeRef = <T>(...refs: React.Ref<T>[]): React.Ref<T> => {
+  const refList = refs.filter(Boolean);
   if (refList.length <= 1) {
     return refList[0];
   }
-
   return (node: T) => {
     refs.forEach(ref => {
       fillRef(ref, node);
     });
   };
-}
+};
 
-export function useComposeRef<T>(...refs: React.Ref<T>[]): React.Ref<T> {
+export const useComposeRef = <T>(...refs: React.Ref<T>[]): React.Ref<T> => {
   return useMemo(
     () => composeRef(...refs),
     refs,
     (prev, next) =>
       prev.length !== next.length || prev.every((ref, i) => ref !== next[i]),
   );
-}
+};
 
-export function supportRef(nodeOrComponent: any): boolean {
+export const supportRef = (nodeOrComponent: any): boolean => {
   const type = isMemo(nodeOrComponent)
     ? nodeOrComponent.type.type
     : nodeOrComponent.type;
@@ -60,11 +58,11 @@ export function supportRef(nodeOrComponent: any): boolean {
     return false;
   }
   return true;
-}
+};
 
-export function supportNodeRef<T = any>(
+export const supportNodeRef = <T = any>(
   node: React.ReactNode,
-): node is React.ReactElement & React.RefAttributes<T> {
+): node is React.ReactElement & React.RefAttributes<T> => {
   if (!isValidElement(node)) {
     return false;
   }
@@ -72,5 +70,4 @@ export function supportNodeRef<T = any>(
     return false;
   }
   return supportRef(node);
-}
-/* eslint-enable */
+};
