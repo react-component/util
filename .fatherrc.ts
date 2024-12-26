8bb45440e72c3f48bfde3ec57a785207caad34ab
栗嘉男 <574980606@qq.com>
2024-12-26 17:53:54 +0800

    fix: fix

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,7 +3,4 @@ import { defineConfig } from 'father';
 export default defineConfig({
   // Locked version only supports 1.0.0
   plugins: ['@rc-component/father-plugin'],
-  targets: {
-    chrome: 85,
-  },
 });
