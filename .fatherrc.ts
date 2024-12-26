88e5e9e81698541a199c9b9d083db217472fa9d5
栗嘉男 <574980606@qq.com>
2024-12-26 15:42:40 +0800

    chore: update father targets

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'father';
 export default defineConfig({
   // Locked version only supports 1.0.0
   plugins: ['@rc-component/father-plugin'],
+  targets: {
+    chrome: 85,
+  },
 });
