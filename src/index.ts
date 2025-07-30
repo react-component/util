f27b1e472e854610d07417ecee7485739a64e28e
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-07-30 14:06:15 +0800

    feat(export)!: restrict to only root path import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,64 @@
-export { default as useEvent } from './hooks/useEvent';
-export { default as useMergedState } from './hooks/useMergedState';
-export { supportNodeRef, supportRef, useComposeRef } from './ref';
 export { default as get } from './utils/get';
 export { default as set, merge } from './utils/set';
-export { default as warning, noteOnce } from './warning';
-export { default as omit } from './omit';
 export { default as toArray } from './Children/toArray';
+export { default as isFragment } from './React/isFragment';
+export { render, unmount } from './React/render';
+export { spyElementPrototype, spyElementPrototypes } from './test/domHook';
+export { default as composeProps } from './composeProps';
+export {
+  default as getScrollBarSize,
+  getTargetScrollBarSize,
+} from './getScrollBarSize';
+export { default as isEqual } from './isEqual';
+export { default as isMobile } from './isMobile';
+export { default as KeyCode } from './KeyCode';
+export { default as omit } from './omit';
+export { default as pickAttrs } from './pickAttrs';
+export type { PickConfig } from './pickAttrs';
+export { default as Portal } from './Portal';
+export type { PortalRef, PortalProps } from './Portal';
+export { default as PortalWrapper } from './PortalWrapper';
+export type { GetContainer, PortalWrapperProps } from './PortalWrapper';
+export { default as proxyObject } from './proxyObject';
+export { default as wrapperRaf } from './raf';
+export {
+  fillRef,
+  composeRef,
+  useComposeRef,
+  supportRef,
+  supportNodeRef,
+  getNodeRef,
+} from './ref';
+export { default as setStyle } from './setStyle';
+export { SetStyleOptions } from './setStyle';
+export { default as warning, noteOnce } from './warning';
+
+// DOM
+export { default as canUseDom } from './Dom/canUseDom';
+export { default as contains } from './Dom/contains';
+export type { ContainerType, Prepend, AppendType } from './Dom/dynamicCSS';
+export { injectCSS, removeCSS, updateCSS } from './Dom/dynamicCSS';
+export { default as findDOMNode, isDOM, getDOM } from './Dom/findDOMNode';
+export {
+  getFocusNodeList,
+  saveLastFocusNode,
+  clearLastFocusNode,
+  backLastFocusNode,
+  limitTabRange,
+} from './Dom/focus';
+export { default as isVisible } from './Dom/isVisible';
+export { default as ScrollLocker } from './Dom/scrollLocker';
+export type { scrollLockOptions } from './Dom/scrollLocker';
+export { inShadow, getShadowRoot } from './Dom/shadow';
+export { isStyleSupport } from './Dom/styleChecker';
+
+// Hooks
+export { default as useEffect } from './hooks/useEffect';
+export { default as useEvent } from './hooks/useEvent';
+export { default as useId } from './hooks/useId';
+export { default as useLayoutEffect } from './hooks/useLayoutEffect';
+export { default as useMemo } from './hooks/useMemo';
+export { default as useMergedState } from './hooks/useMergedState';
+export { default as useMobile } from './hooks/useMobile';
+export { default as useState } from './hooks/useState';
+export { default as useSyncState } from './hooks/useSyncState';
