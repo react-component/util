9c906681e5bf18c3d9172ee478a8041096bbe2ea
二货机器人 <smith3816@gmail.com>
2025-09-02 16:49:06 +0800

    chore: default export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as useEvent } from './hooks/useEvent';
 export { default as useMergedState } from './hooks/useMergedState';
+export { default as useControlledState } from './hooks/useControlledState';
 export { supportNodeRef, supportRef, useComposeRef } from './ref';
 export { default as get } from './utils/get';
 export { default as set, merge } from './utils/set';
