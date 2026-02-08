d97e5279372917c73ba1c92f0c0ba1b597d64db8
lijianan <574980606@qq.com>
2026-02-08 22:00:57 +0800

    chore: rm eslint-disable

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -1,14 +1,12 @@
-/* eslint-disable @typescript-eslint/ban-types */
-/* eslint-disable react-hooks/exhaustive-deps */
 import * as React from 'react';
 
-function useEvent<T extends ((...args: any[]) => any) | undefined>(
+const useEvent = <T extends ((...args: any[]) => any) | undefined>(
   callback: T,
 ): undefined extends T
   ? (
       ...args: Parameters<NonNullable<T>>
     ) => ReturnType<NonNullable<T>> | undefined
-  : T {
+  : T => {
   const fnRef = React.useRef<T | undefined>(callback);
   fnRef.current = callback;
 
@@ -18,6 +16,6 @@ function useEvent<T extends ((...args: any[]) => any) | undefined>(
   );
 
   return memoFn;
-}
+};
 
 export default useEvent;
